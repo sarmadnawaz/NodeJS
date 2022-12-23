0b40ee2fabfa85326d76776460006c8b2dc351a6
sarmadnawaz <sarmadnawazkhan123@gmail.com>
2022-12-23 19:47:28 +0500

    impelemented middlewares for sanitizing data, Security HTTP headers, and preventing parameters pollution

--- natours-project/app.js
+++ natours-project/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const appError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/ErrorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const app = express();
+
+app.use(helmet());
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +28,18 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Data Snaitization
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ['duration', 'ratingsQuantity'],
+  })
+);
+app.use(express.json({ limit: '10kb' }));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
